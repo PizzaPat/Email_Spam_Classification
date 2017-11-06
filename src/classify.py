35c8b01791555d3590eb73062ce54ad23960b9d7
Pat <patpongtana@gmail.com>
2017-11-06 11:20:11 -0800

    update test case

--- src/classify.py
+++ src/classify.py
@@ -3,10 +3,9 @@ from collections import Counter
 import re
 print("Start processing")
 
-counts = list()
-i=0
-
 words = list()
+wordsSpam = list()
+wordsHam = list()
 lineNum = 0;
 
 with open('../spam_train.txt') as f:
@@ -20,22 +19,55 @@ with open('../spam_train.txt') as f:
 			words =  tmpList + words
 			lineNum = lineNum + 1
 
-'''
-with open('../spam_test.txt') as fp:
-	for i, line in enumerate(fp):
-		if i < 4000:
-			line = fp.readlines()
-			print(line)
-			tmpList = line.lower()
-			tmpList = re.sub('[^a-z\ \']+', " ", tmpList)
-			words = list(tmpList.split()) + words
-fp.close()
-'''
+			if line[0] == '0':
+				wordsHam = tmpList + wordsHam
+			elif line[0] == '1':
+				wordsSpam = tmpList + wordsSpam
+			else:
+				pass
 
+''' count all the words that exist in the list and then
+	transform them into list once again '''
 d = dict(Counter(words))
+d2 = dict(Counter(wordsHam))
+d3 = dict(Counter(wordsSpam))
 
-print('------------Done------------')
+
+print('///// Test case for keyword \'aniwai\'')
+
+print('--------------------------Total from 4000------------')
 for key, value in d.items():
-	if value >= 30:
+	if value >= 1 and key == 'anywai':
 		print('Keyword:', key, ' Value:', value)
+		keyHamSpam = key
+		valueHamSpam = value
+
+print('--------------------------Words Ham------------------')
+
+for key, value in d2.items():
+	if value >= 1 and key == 'anywai':
+		print('Keyword:', key, ' Value:', value)
+		keyHam = key
+		valueHam = value
+
+print('--------------------------Words Spam-----------------')
+
+for key, value in d3.items():
+	if value >= 1 and key == 'anywai':
+		print('Keyword:', key, ' Value:', value)
+		keySpam = key
+		valueSpam = value
+
+
+print('-----------Probability of Ham of \'anywai\' is-------------')
+
+print('---------Ham------------')
+print('computation: ',valueHam,'/',valueHamSpam)
+print('---------Spam-----------')
+print('computation: ',valueSpam,'/',valueHamSpam)
+print('\n\n\n\n')
+result = valueHam /valueHamSpam
+print('P(Ham)=', result)
+result = valueSpam /valueHamSpam
+print('P(Spam)=', result)
 
